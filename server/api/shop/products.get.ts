9170d4251ea8a6d186c53ff2db68f690ad31cf5e
LittleSheep <hi@littlesheep.me>
2026-07-13 02:13:11 +0800

    :bug: Fix product page

--- server/api/shop/products.get.ts
+++ server/api/shop/products.get.ts
@@ -1,10 +1,17 @@
 import { snFetch } from "~~/server/utils/sn-api";
 import { snakeToCamel } from "~~/server/utils/snakeToCamel";
+import { getAdminToken } from "~~/server/utils/solarAccount";
 
 export default defineEventHandler(async (event) => {
+  const token = await getAdminToken();
+  if (!token) {
+    throw createError({ statusCode: 500, statusMessage: "API not configured" });
+  }
+
   const products = await snFetch<any[]>(
     event,
     "/develop/apps/goatshed/products",
+    { token },
   );
 
   return (products || []).map((p) => {
